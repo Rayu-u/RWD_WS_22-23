39401ccb1732baef95a42cd167db768f06d3cfb7
Laura Fabricius <62756038+Rayu-u@users.noreply.github.com>
2023-01-10 10:48:34 +0100

    project set up + experiments w buttons and first stripes experiments

---
+++ Umsetzung/modules/button.js
@@ -0,0 +1,9 @@
+/**
+ *
+ *
+ * @param {string} cssColor as the color string
+ * @param {string | string[]} text The text content of the button being made. In an array if should span multiple lines?
+ */
+const createButton = (activeColor, inactiveColor, text) => {
+  const element = document.createElement("div");
+};
